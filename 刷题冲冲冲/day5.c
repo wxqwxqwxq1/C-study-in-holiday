70790aa1eb8f6a9377e60c5ac571a28af0a57ab2
wxqwxqwxq1 <weixinqiang32@gmail.com>
2022-08-05 23:24:54 +0800

    08-5

--- 刷题冲冲冲/day5.c
+++ 刷题冲冲冲/day5.c
@@ -117,24 +117,32 @@
 //	}
 //}
 //自动关机
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-int main()
-{
-	char input[20] = { 0 };
-	system("shutdown -s -t 60");
-	//msg% username% / w / v 这是一个提示框
-again:
-	system("msg %username% /w /v 请注意，你的电脑将在一分钟后关机，如果输入：我是猪，就取消关机");
-	printf("请输入>：");
-	scanf("%s", input);
-	if (strcmp(input, "我是猪") == 0)
-	{
-		system("shutdown -a");
-		system("msg %username% /w /v 你是猪，哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈!!!!!!!!");
-		system("msg %username% /w /v 电脑已取消关机");
-	}
-	else goto again;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 60");
+//	//msg% username% / w / v 这是一个提示框
+//again:
+//	//system("msg %username% /w /v 请注意，你的电脑将在一分钟后关机，如果输入：我是猪，就取消关机");
+//	printf("请注意，你的电脑将在一分钟后关机，如果输入：我爱你，就取消关机，请输入>：");
+//	scanf("%s", input);
+//	if (strcmp(input, "我爱你") == 0)
+//	{
+//		system("shutdown -a");
+//		//system("msg %username% /w /v 你是猪，哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈!!!!!!!!");
+//		//system("msg %username% /w /v 电脑已取消关机");
+//	}
+//	else goto again;
+//}
+//int i = 0, j = 0;
+//for (; i < 10; i++)
+//{
+//	for (; j < 10; j++)
+//	{
+//		printf("hehe\n");
+//	}
+//}
